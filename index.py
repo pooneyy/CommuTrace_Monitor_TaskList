da9961c2995dc2a83c5ee1439f9963ec468f8333
Qiuyelin <85266337+pooneyy@users.noreply.github.com>
2023-03-19 10:52:35 +0800

    Update index.py (0.1.2)
    1、当无订单时崩溃的问题；
    2、当订单数>1时，订单创建时间解析错误的问题；
    3、修复其它错误。

--- index.py
+++ index.py
@@ -4,7 +4,7 @@
 Author       : Qiuyelin
 Date         : 2023-03-14 20:08:52
 LastEditors  : Qiuyelin 85266337+pooneyy@users.noreply.github.com
-LastEditTime : 2023-03-18 20:52:16
+LastEditTime : 2023-03-19 10:46:50
 FilePath     : /CommuTrace_Monitor_TaskList/index.py
 Description  : 共迹算力平台_监听任务列表
 
@@ -19,7 +19,7 @@ import datetime, pytz, time
 import sys
 import requests
 
-VERSION = '0.1.1' # 2023.03.16
+VERSION = '0.1.2' # 2023.03.19
 CONFIG_VERSION = 0.1
 HOST = "http://39.101.72.182/index.php"
 
@@ -121,27 +121,30 @@ def analyzingAllorder(allorder):
     '''分析订单页面'''
     if '请在登录后操作' in allorder:return 0
     else:
-        taskData = re.findall(r'<td>[\s\S]+<\/td>', allorder)[0]
-        createTime = re.findall(r"(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)", taskData)[0][0]
-        taskData = re.sub(r"(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)", f"{createTime}_" ,taskData)
-        taskData = taskData.replace("\"" , "\'")
-        taskData = taskData.replace(' ' , '')
-        taskData = taskData.replace('\t' , '')
-        taskData = taskData.replace('\r\n' , '')
-        # if 'tr' in taskData: taskData = re.sub(r"(<.?tr>)", r"" ,taskData)
-        taskData = re.sub(r"(<.?tr>)", r"" ,taskData)
-        taskData = taskData.replace('</td><td>' , '</td>,<td>')
-        taskData = re.sub(r"(<.?td>)", r'"' ,taskData)
-        taskData = json.loads("[" + taskData + "]")
-        n = 8 # 分割列表，每8项为一组
-        taskValue = []
-        taskKeys = ['orderID', 'orderName', 'orderDetail', 'createTime', 'unitIncome', 'completed_and_Total', 'number_of_Participants', 'applyOrder']
-        taskList = []
-        for i in range(0, len(taskData), n):
-            taskValue = taskData[i:i + n]
-            taskDict = dict(zip(taskKeys, taskValue))
-            taskList.append(taskDict)
-        return taskList
+        taskData = re.findall(r'<td>[\s\S]+<\/td>', allorder)
+        if not taskData:return 0
+        else:
+            taskData = taskData[0]
+            createTime = re.findall(r"(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)", taskData)
+            for i in createTime:
+                taskData = taskData.replace(f"{i[0]}" , f"{i[0]}_")
+            taskData = taskData.replace("\"" , "\'")
+            taskData = taskData.replace(' ' , '')
+            taskData = taskData.replace('\t' , '')
+            taskData = taskData.replace('\r\n' , '')
+            taskData = re.sub(r"(<.?tr>)", r"" ,taskData)
+            taskData = taskData.replace('</td><td>' , '</td>,<td>')
+            taskData = re.sub(r"(<.?td>)", r'"' ,taskData)
+            taskData = json.loads("[" + taskData + "]")
+            n = 8 # 分割列表，每8项为一组
+            taskValue = []
+            taskKeys = ['orderID', 'orderName', 'orderDetail', 'createTime', 'unitIncome', 'completed_and_Total', 'number_of_Participants', 'applyOrder']
+            taskList = []
+            for i in range(0, len(taskData), n):
+                taskValue = taskData[i:i + n]
+                taskDict = dict(zip(taskKeys, taskValue))
+                taskList.append(taskDict)
+            return taskList
 
 def taskList_To_Msg(taskList):
     msg = f'''
@@ -153,39 +156,37 @@ def taskList_To_Msg(taskList):
     return msg
 
 def loop(config):
-    cookies = login(config)
     try:
+        cookies = login(config)
         while True:
             taskList = analyzingAllorder(getAllorder(cookies))
-            if taskList == 0 :
-                cookies = login(cookies)
-                continue
-            i = [i['orderID'] for i in taskList] # 列表，临时存储订单ID用于寻找最大的ID
-            latestOrderID = int(max(i))
-            if latestOrderID > config['latestOrderID']:
-                config['latestOrderID'] = latestOrderID
-                msg = f"#### 有新订单。当前最新是 {latestOrderID}\n"
-                msg += taskList_To_Msg(taskList)
-                sendMsg(config, msg)
-                saveConfig(config)
-                print(f"{taskList_To_Msg(taskList)}\n")
-            print(f"刷新于 {timeStamp_To_dateTime(time.time())}\r",end='')
-            time.sleep(30)
+            if  taskList == 0 :cookies = login(cookies)
+            else:
+                i = [i['orderID'] for i in taskList] # 列表，临时存储订单ID用于寻找最大的ID
+                latestOrderID = int(max(i))
+                if latestOrderID > config['latestOrderID']:
+                    config['latestOrderID'] = latestOrderID
+                    msg = f"#### 有新订单。当前最新是 {latestOrderID}\n"
+                    msg += taskList_To_Msg(taskList)
+                    sendMsg(config, msg)
+                    saveConfig(config)
+                    print(f"{taskList_To_Msg(taskList)}\n")
+                print(f"刷新于 {timeStamp_To_dateTime(time.time())}\r",end='')
+                time.sleep(30)
+    except TypeError:loop(config)
+    except KeyError:loop(config)
     except KeyboardInterrupt:print("结束")
-
-def main():
-    if 'linux' in sys.platform: sys.stdout.write(f"\x1b]2;监听共迹任务列表 - 版本 {VERSION}\x07")
-    elif 'win' in sys.platform: os.system(f"title 监听共迹任务列表 - 版本 {VERSION}")
-    try:
-        config = loadConfig()
-        try:loop(config)
-        except TypeError:loop(config)
-        except KeyError:loop(config)
-        except requests.exceptions.ConnectionError:
+    except requests.exceptions.ConnectionError:
+        try:
             print("与服务器连接中断，30秒后重试")
             time.sleep(30)
             loop(config)
         except KeyboardInterrupt:print("结束")
+
+def main():
+    if 'linux' in sys.platform: sys.stdout.write(f"\x1b]2;监听共迹任务列表 - 版本 {VERSION}\x07")
+    elif 'win' in sys.platform: os.system(f"title 监听共迹任务列表 - 版本 {VERSION}")
+    try:loop(loadConfig())
     except FileNotFoundError:
         try:init()
         except KeyboardInterrupt:print("\n退出，取消初始化")
